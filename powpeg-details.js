4d3384a8e9c555f2e970495f5889ef81eeeb9ff2
Marcos <marcos@iovlabs.org>
2022-12-07 16:34:03 -0300

    Create a method to simplify obtaining the active powpeg redeem script

--- powpeg-details.js
+++ powpeg-details.js
@@ -1,6 +1,6 @@
-const Bridge = require('@rsksmart/rsk-precompiled-abis').bridge;
+const bridge = require('@rsksmart/rsk-precompiled-abis').bridge;
+const redeemScriptParser = require('powpeg-redeemscript-parser');
 const getFederationPublicKeys = require('./pegnatory-public-keys');
-const RedeemScriptParser = require('powpeg-redeemscript-parser');
 const getFedBtcKeys = require('./federation-btc-keys');
 
 class PowpegDetails {
@@ -21,36 +21,41 @@ class PowpegDetails {
     }
 }
 
-module.exports = async (web3, networkSettings) => {
-    const bridge = Bridge.build(web3);
-    const federationSize = await bridge.methods.getFederationSize().call();
-    const federationThreshold = await bridge.methods.getFederationThreshold().call();
-    const federationAddress = await bridge.methods.getFederationAddress().call();
-    const pegnatoryPublicKeys = await getFederationPublicKeys(bridge);
-    const btcPublicKeys = getFedBtcKeys(pegnatoryPublicKeys);
-    const federationCreationBlockNumber = await bridge.methods.getFederationCreationBlockNumber().call();
-
+const getActivePowpegRedeemScript = async(bridgeInstance, networkSettings, federationCreationBlockNumber) => {
     let redeemScript;
     if (federationCreationBlockNumber >= networkSettings.getNetworkUpgradesActivationHeights().getActivationHeight('hop')) {
-        redeemScript = (await bridge.methods.getActivePowpegRedeemScript().call()).substring(2);
-    } else if (federationCreationBlockNumber >= networkSettings.getNetworkUpgradesActivationHeights().getActivationHeight('iris')
-            && federationCreationBlockNumber < networkSettings.getNetworkUpgradesActivationHeights().getActivationHeight('hop')) {
-        const federationAddress = await bridge.methods.getFederationAddress().call();
-        redeemScript = RedeemScriptParser.getPowpegRedeemScript(btcPublicKeys).toString('hex');
-        if (federationAddress != RedeemScriptParser.getAddressFromRedeemScript(networkSettings.getNetworkName(), redeemScript)) {
-            redeemScript = RedeemScriptParser.getP2shErpRedeemScript(
-                btcPublicKeys, 
-                networkSettings.getErpDetails().getErpPublicKeys(), 
-                networkSettings.getErpDetails().getCsvValue()
-            ).toString('hex');
-
-            if (federationAddress != RedeemScriptParser.getAddressFromRedeemScript(networkSettings.getNetworkName(), redeemScript)) {
-                throw new Error("RedeemScript could not be parsed");
-            }
-        }
-    } else {
-        redeemScript = RedeemScriptParser.getPowpegRedeemScript(btcPublicKeys).toString('hex');
+        // Use the Bridge method added in Hop that returns the active powpeg redeem script
+        return (await bridgeInstance.methods.getActivePowpegRedeemScript().call()).substring(2);
     }
+    if (federationCreationBlockNumber >= networkSettings.getNetworkUpgradesActivationHeights().getActivationHeight('iris')) {
+        // Try getting the p2sh erp federation redeem script
+        let redeemScript = redeemScriptParser.getP2shErpRedeemScript(
+            btcPublicKeys, 
+            networkSettings.getErpDetails().getErpPublicKeys(), 
+            networkSettings.getErpDetails().getCsvValue()
+        ).toString('hex');
+
+        // Compare the address obtained from the redeem script to the one returned by the Bridge
+        // If they don't match then it's probably because an ERP federation has not been created yet
+        const federationAddress = await bridgeInstance.methods.getFederationAddress().call();
+        if (federationAddress == redeemScriptParser.getAddressFromRedeemScript(networkSettings.getNetworkName(), redeemScript)) {
+            return redeemScript;
+        }
+    }
+    
+    // Return the standard redeem script by default, iris not active or ERP federation not created yet
+    return redeemScriptParser.getPowpegRedeemScript(btcPublicKeys).toString('hex');
+}
+
+module.exports = async (web3, networkSettings) => {
+    const bridgeInstance = bridge.build(web3);
+    const federationSize = await bridgeInstance.methods.getFederationSize().call();
+    const federationThreshold = await bridgeInstance.methods.getFederationThreshold().call();
+    const federationAddress = await bridgeInstance.methods.getFederationAddress().call();
+    const pegnatoryPublicKeys = await getFederationPublicKeys(bridgeInstance);
+    const btcPublicKeys = getFedBtcKeys(pegnatoryPublicKeys);
+    const federationCreationBlockNumber = await bridgeInstance.methods.getFederationCreationBlockNumber().call();
+    const redeemScript = await getActivePowpegRedeemScript(bridgeInstance, networkSettings, federationCreationBlockNumber);
 
     return new PowpegDetails(
         Number(federationSize), 
